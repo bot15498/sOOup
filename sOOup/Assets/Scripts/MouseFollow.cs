f76c0cf1b193fd5a3e6b6d89e660d2845c639c21
skyler <konata.izumi20@gmail.com>
2021-05-01 11:02:53 -0400

    Add branding scenario

---
+++ sOOup/Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    public GameObject stampPrefab;
+    public GameObject spawnpoint;
+    public BoxCollider2D goodBounds;
+    public BoxCollider2D allBounds;
+    [SerializeField]
+    private bool lastClick = false;
+    private WinController wc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wc = GetComponent<WinController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 4));
+        bool click = Input.GetKey(KeyCode.Mouse0);
+        if (click && lastClick != click)
+        {
+            if (goodBounds.bounds.Contains(spawnpoint.transform.position))
+            {
+                GameObject stamp = Instantiate(stampPrefab);
+                stamp.transform.position = spawnpoint.transform.position;
+                lastClick = click;
+                wc.SetWin();
+            }
+            else if (allBounds.bounds.Contains(spawnpoint.transform.position))
+            {
+                GameObject stamp = Instantiate(stampPrefab);
+                stamp.transform.position = spawnpoint.transform.position;
+                lastClick = click;
+                wc.SetLose();
+            }
+        }        
+    }
+}
---
+++ sOOup/Assets/Scripts/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Image timerBar;
+    public float timerLength = 20f;
+
+    private WinController wc;
+    [SerializeField]
+    private float timeElapsed = 0f;
+    private float maxwidth = 0f;
+    private float height = 0f;
+
+    void Start()
+    {
+        wc = FindObjectOfType<WinController>();
+        maxwidth = timerBar.rectTransform.sizeDelta.x;
+        height = timerBar.rectTransform.sizeDelta.y;
+    }
+
+    void Update()
+    {
+        if(wc == null)
+        {
+            wc = FindObjectOfType<WinController>();
+        }
+        
+        if(!wc.isAnimating)
+        {
+            timeElapsed += Time.deltaTime;
+
+            timerBar.rectTransform.sizeDelta = new Vector2(maxwidth * (timerLength - timeElapsed) / timerLength, height);
+            if(timeElapsed > timerLength)
+            {
+                wc.SetLose();
+            }
+        }
+    }
+}
