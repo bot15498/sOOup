859b5cf6dc30c74acb2688986a4c61ac01fa4776
AAYFlower <44514581+AAYFlower@users.noreply.github.com>
2021-05-01 04:12:54 -0500

    3 soups
    asdf

---
+++ sOOup/Assets/FaceBean.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceBean : MonoBehaviour
+{
+
+
+    public GameObject beans;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vectorToTarget = beans.transform.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+    }
+}
