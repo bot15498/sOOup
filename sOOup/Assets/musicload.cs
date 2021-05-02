e174acbe364443a06c4d425a8ffedc386032d45f
AAYFlower <44514581+AAYFlower@users.noreply.github.com>
2021-05-02 15:50:31 -0500

    sdf

---
+++ sOOup/Assets/musicload.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicload : MonoBehaviour
+{
+    public static musicload Instance;
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
